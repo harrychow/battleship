07339b8b5b2d4b492bb28195eba792725df0b475
Harry Chow <harry.chow@crick.ac.uk>
2018-10-10 18:18:50 +0100

    -added phpunit to composer install -PlayController now uses form helper and request object to decide what to display -twig renders form and table

---
+++ battleship/tests/Entity/BoardTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class BoardTest extends TestCase
+{
+    public function testSomething()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testConstruct()
+    {
+
+    }
+
+    public function testAddMiss()
+    {
+
+    }
+
+    public function testAddHits()
+    {
+
+    }
+
+    public function testGetAttempts()
+    {
+
+    }
+
+    public function testGetSuccessHits()
+    {
+
+    }
+
+    public function testConstructWithBlankBoard()
+    {
+
+    }
+
+    public function testConstructWithSavedValues()
+    {
+
+    }
+
+
+}
